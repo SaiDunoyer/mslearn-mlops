da15008bd15e024a402a53bef4e93d48cdeec50f
Sai Dunoyer <sai.dunoyer@gmail.com>
2023-09-26 12:19:37 +0200

    MLFlow autologging

--- src/model/train.py
+++ src/model/train.py
@@ -6,15 +6,19 @@ import os
 
 import pandas as pd
 import numpy as np
+import mlflow
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
+
+
 # define functions
 
 def main(args):
-    # TO DO: enable autologging
-
+    # enable autologging
+    mlflow.autolog()
+    
     # read data
     df = get_csvs_df(args.training_data)
 
